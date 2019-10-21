62a869d0fec845ebb8a6e1f616376b82c0ea1f6a
william assaf <6641470+williamadba@users.noreply.github.com>
2019-10-20 23:27:07 -0500

    work in progress

--- PASS Speaker Idol 2019/lab - nonsequentialguid.sql
+++ PASS Speaker Idol 2019/lab - nonsequentialguid.sql
@@ -1,3 +1,15 @@
+--Conditions that trigger a nonsequential "sequential" guid
+ --- Failover to another machine may trigger in FC or AG
+ --- Upgrade or Migrate the database to new hardware, new platform
+ --- Change tier of Azure SQL DB 
+ --- Re-image an Azure VM with ephemeral OS 
+ --- Since the C++ function UuidCreateSequential is based on "the MAC address of a machine's Ethernet card", changing or replacing the NIC may trigger,
+ ---  or anything that changes the NIC MAC may trigger.
+ ------ You cannot specify the MAC address of a new Azure network interface to prevent this on a new machine!
+ ------ more info: https://docs.microsoft.com/en-us/windows/win32/api/rpcdce/nf-rpcdce-uuidcreatesequential?redirectedfrom=MSDN
+ ------ "Therefore you should never use this UUID to identify an object that is not strictly local to your computer."
+
+/*
 use master
 go
 CREATE DATABASE [make_ints_not_guids]
@@ -13,19 +25,23 @@ use [make_ints_not_guids]
 go
 --drop table if exists dbo.nonsequentialguid
 go
-create table dbo.nonsequentialguid
-(id uniqueidentifier not null CONSTRAINT DF_nonseq_id DEFAULT newsequentialid()
-, whenobserved datetimeoffset(2)  not null CONSTRAINT DF_nonseq_when DEFAULT sysdatetimeoffset() 
-, whatever char(1) not null
-, constraint pk_nonseq primary key (id) --WITH (OPTIMIZE_FOR_SEQUENTIAL_KEY = on)--SQL 2019+
+CREATE TABLE dbo.nonsequentialguid
+(id uniqueidentifier NOT NULL CONSTRAINT DF_nonseq_id DEFAULT newsequentialid()
+, whenobserved datetimeoffset(2) NOT NULL CONSTRAINT DF_nonseq_when DEFAULT sysdatetimeoffset() 
+, constraint pk_nonseq primary key (id) WITH (OPTIMIZE_FOR_SEQUENTIAL_KEY = on) --SQL 2019+ only
 ) 
 go
 
+*/
+
 --Run the below command after reboots, failovers, etc. You will eventually start writing nonsequential "sequential" guids.
 insert into dbo.nonsequentialguid (whatever) values ('a')
-go
 
---Check for nonsequential "sequential" GUIDs. All will trigger if it happens.
+/*
+Try to trigger the nonsequential sequential guid. It's not enough to reboot, it has to be a chance to the NIC MAC. See list above.
+*/
+
+--Check for nonsequential "sequential" GUIDs. All three of the below will show results when a nonsequential sequential guid is inserted.
 select n1.id, n1.whenobserved, n2.id, n2.whenobserved  from nonsequentialguid n1
 , nonsequentialguid n2
 where n1.id > n2.id and n1.whenobserved < n2.whenobserved -- when an id is greater than another record, but its when is less than the other record's when. It's happened.
@@ -41,12 +57,4 @@ select id, whenobserved
  order by id asc -- when first records in this sort order will have been inserted after later records, it's happened.
 
 
- --Conditions that trigger a nonsequential "sequential" guid
- --- Failover to another machine may trigger in FC or AG
- --- Upgrade or Migrate the database to new hardware, new platform
- --- Re-image an Azure VM with ephemeral OS may trigger
- --- Since the C++ function UuidCreateSequential is based on the MAC address of a machine's Ethernet card, changing or replacing the network interface may trigger,
- ------ You cannot specify the MAC address of a new Azure network interface to prevent this on a new machine!
- ------ more info: https://docs.microsoft.com/en-us/windows/win32/api/rpcdce/nf-rpcdce-uuidcreatesequential?redirectedfrom=MSDN
- ------ "Therefore you should never use this UUID to identify an object that is not strictly local to your computer."
-
+ 
