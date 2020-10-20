13da97d8d609002ff432189d5b98407f1005fc47
william assaf <6641470+williamadba@users.noreply.github.com>
2020-10-19 22:52:45 -0500

    progress 10/19

---
+++ PASS Summit 2020/4 - fix orphaned sid.sql
@@ -0,0 +1,57 @@
+--Only works for SQL 2005 SP2 or later!
+/** GENERATE TSQL TO FIX ORPHANS **/
+Select 
+		DBUser_Name	=	dp.name
+	,	DBUser_SID	=	dp.sid
+	,	Login_Name	=	sp.name
+	,	Login_SID	=	sp.sid
+	,	SQLtext		=	'ALTER USER [' + dp.name + '] WITH LOGIN = [' + ISNULL(sp.name, '???') + ']'
+	from sys.database_principals dp
+	left outer join sys.server_principals sp
+	on dp.name = sp.name 
+	where 
+		dp.is_fixed_role = 0
+	and sp.sid <> dp.sid 
+	and dp.principal_id > 1
+	and dp.sid is not null 
+	and dp.sid <> 0x0
+	order by dp.name
+go
+
+/* multi-database
+
+exec sp_MSforeachdb N'use [?];
+Select [?]=	''?'', 	''ALTER USER ['' + dp.name + ''] WITH LOGIN = ['' + dp.name + '']'', *
+	from sys.database_principals dp
+	inner join sys.server_principals sp
+	on dp.name COLLATE SQL_Latin1_General_CP1_CI_AS = sp.name COLLATE SQL_Latin1_General_CP1_CI_AS 
+	where 
+		dp.is_fixed_role = 0
+	and (dp.sid is not null and dp.sid <> 0x0)	
+	and sp.sid <> dp.sid 
+	and dp.principal_id > 1
+	order by dp.name
+'
+*/
+
+
+/*
+
+--Lab: Create orphaned SID
+
+USE [master]
+GO
+CREATE LOGIN [test] WITH PASSWORD=N'test', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=OFF, CHECK_POLICY=OFF
+GO
+
+USE w
+GO
+CREATE USER test
+GO
+
+
+test	0x1E1EEF7790E11745B42B9A33083DFF55	NULL	NULL	ALTER USER [test] WITH LOGIN = [test]
+
+ALTER USER [test] WITH LOGIN = [test]
+
+*/
